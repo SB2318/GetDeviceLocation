c90ae311fba09611be84a2c11509d380eb14bbe9
SB2318 <susmitabhattacharya2023@gmail.com>
2022-09-29 00:40:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/locationdemo/MainActivity.java
@@ -0,0 +1,181 @@
+package com.example.locationdemo;
+
+import static android.location.LocationManager.*;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        grantPermission();
+
+        checkLocationIsEnableOrNot(); // This will redirect to us to the location settings
+
+        getUserRole();
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+    }
+
+    private void getUserRole() {
+
+        LocationManager locationManager;
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        }catch(Exception e){
+             e.printStackTrace();
+        }
+    }
+
+    private void checkLocationIsEnableOrNot() {
+
+        LocationManager lm= (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsEnabled= false;
+        boolean networkEnabled= false;
+
+        try{
+            gpsEnabled= lm.isProviderEnabled(GPS_PROVIDER);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        try{
+
+            networkEnabled= lm.isProviderEnabled(NETWORK_PROVIDER);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        if(!gpsEnabled && !networkEnabled){
+
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Enable GPS Service")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //This intent redirect us to the location settings
+                            // If GPS is disabled, this dialogue will show
+                            startActivity(new Intent(
+                                    Settings.ACTION_LOCATION_SOURCE_SETTINGS
+                            ));
+                        }
+                    })
+                    .setNegativeButton("Cancel",null)
+                    .show();
+        }
+
+    }
+
+    private void grantPermission() {
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+          != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    },100);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+        Geocoder geocoder= new Geocoder(
+                getApplicationContext(),
+                Locale.getDefault()
+        ) ;
+
+        try {
+            List<Address> addresses= geocoder.getFromLocation(
+                    location.getLatitude(), location.getLongitude(), 1
+            );
+
+            String country= addresses.get(0).getCountryName();
+            String state= addresses.get(0).getAdminArea();
+            String city= addresses.get(0).getLocality();
+            String pinCode= addresses.get(0).getPostalCode();
+            String address= addresses.get(0).getAddressLine(0);
+
+            Log.d("TAG",city+" ,"+state);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
